a60204be79d38620aa558702f75b27ca094c8908
MartinMedinaRuvian <martinjesusmr@ufps.edu.co>
2020-06-23 17:42:48 -0500

    Agregue mas reglas del negocio y implemente las reglas en el modelo principal(Tienda)

--- src/Control/ControlVista.java
+++ src/Control/ControlVista.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Control;
 
-/**
- *
- * @author Admin
- */
 public class ControlVista {
-    
+   private ControlNegocio control = new ControlNegocio();
+   
+ //  public boolean guardarProducto(String nombre, String codigo, int cantidad, )
 }
--- src/Negocio/Tienda.java
+++ src/Negocio/Tienda.java
@@ -1,5 +1,6 @@
 package Negocio;
 
+import Interface.ReglasNegocio;
 import java.util.Iterator;
 import java.util.TreeSet;
 
@@ -7,11 +8,11 @@ import java.util.TreeSet;
  *
  * @author Andrey
  */
-public class Tienda {
+public class Tienda implements ReglasNegocio {
 
-    TreeSet<Producto> productos = new TreeSet<Producto>();
-    TreeSet<Cliente> clientes = new TreeSet<Cliente>();
-    TreeSet<Proveedor> proveedores = new TreeSet<Proveedor>();
+    private TreeSet<Producto> productos = new TreeSet<>();
+    private TreeSet<Cliente> clientes = new TreeSet<>();
+    private TreeSet<Proveedor> proveedores = new TreeSet<>();
 
     /*
     Andrey men cuando es usted declara un metodo la primera letra es en minuscula
@@ -22,15 +23,18 @@ public class Tienda {
     Hay que realizar un metodo para buscar un producto
     terminelo Andrey por favor
      */
+    @Override
     public Producto buscarProducto(String codigo) {
         return null;
     }
 
+    @Override
     public boolean guardarProducto(Producto producto) {
         productos.add(producto);
         return true;
     }
 
+    @Override
     public boolean venderProducto(Producto producto, Cliente cliente, int cant) {
 
         if (cant >= producto.getCantProducto()) {
@@ -46,32 +50,58 @@ public class Tienda {
         }
         return false;
     }
-    
-    public boolean comprarProducto(Producto producto, Proveedor proveedor, int cantidad){
-        return false;
-    }
-    
-    
-    public boolean comprobarStock(Producto producto){
+
+    @Override
+    public boolean comprarProducto(Producto producto, Proveedor proveedor, int cantidad) {
         return false;
     }
 
+    @Override
+    public boolean comprobarStock(Producto producto) {
+        return false;
+    }
+
+    @Override
     public Cliente buscarCliente(int cedula) {
         return null;
     }
 
+    @Override
     public boolean guardarCliente(Cliente cliente) {
         clientes.add(cliente);
         return true;
     }
 
+    @Override
     public Proveedor buscarProveedor(String NIT) {
         return null;
     }
 
+    @Override
     public boolean guardarProveedor(Proveedor proveedor) {
         proveedores.add(proveedor);
         return false;
+    }
+
+    @Override
+    public String verProductos() {
+        String msg = "";
+        
+        for(Iterator<Producto> p = productos.iterator(); p.hasNext();){
+           msg += p.toString();
+        }
+       
+        return msg;
+    }
+
+    @Override
+    public String verClientes() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String verProveedores() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
 }
